322f49441d97f7b8d0ab2f709d7c0ce25932d81e
Gabe Finch and Kathryn Carr <kathrynelizabethcarr@gmail.com>
2015-03-02 11:35:52 -0800

    Add application and stroy templates

--- models/router.js
+++ models/router.js
@@ -1,3 +1,6 @@
 NutriaFeed.Router.map( function() {
-  this.resource('home');
+  this.resource('application');
+  this.resource('home', {path: '/'});
+  this.resource('babynappers');
+  this.resource('celebs');
 });
